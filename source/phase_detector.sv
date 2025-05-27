f427657fd5da1f1480d737e1d65779410aa0f2fa
Foez Ahmed <foez.official@gmail.com>
2025-05-27 19:34:14 +0600

    modified:   source/phase_detector.sv new file:   source/pll.sv new file:   source/pll_tb.sv modified:   source/vco.sv

--- source/phase_detector.sv
+++ source/phase_detector.sv
@@ -1,29 +1,44 @@
 module phase_detector (
+    input  logic arst_ni,
     input  logic clk_ref_i,
     input  logic clk_pll_i,
-    input  logic arst_ni,
-    output logic freq_p_o,
-    output logic freq_n_o
+    output logic freq_incr_o,
+    output logic freq_decr_o
 );
+
+  logic clk_ref_posedge;
+  logic clk_ref_negedge;
+
+  logic clk_pll_posedge;
+  logic clk_pll_negedge;
 
   logic comb_arst_ni;
 
-  always_ff @(posedge clk_ref_i or negedge comb_arst_ni) begin
-    if (~comb_arst_ni) begin
-      freq_p_o <= '0;
-    end else begin
-      freq_p_o <= '1;
-    end
-  end
+  `define PHASE_DETECTOR_FLIPFLOP_GENENRATOR(__CLK__, __EDGE__)                  \
+  always_ff @(``__EDGE__``edge clk_``__CLK__``_i or negedge comb_arst_ni) begin  \
+    if (~comb_arst_ni) begin                                                     \
+      clk_``__CLK__``_``__EDGE__``edge <= '0;                                    \
+    end else begin                                                               \
+      clk_``__CLK__``_``__EDGE__``edge <= '1;                                    \
+    end                                                                          \
+  end                                                                            \
 
-  always_ff @(posedge clk_pll_i or negedge comb_arst_ni) begin
-    if (~comb_arst_ni) begin
-      freq_n_o <= '0;
-    end else begin
-      freq_n_o <= '1;
-    end
-  end
 
-  always_comb comb_arst_ni = ~(freq_p_o & freq_n_o) & arst_ni;
+  `PHASE_DETECTOR_FLIPFLOP_GENENRATOR(ref, pos)
+  `PHASE_DETECTOR_FLIPFLOP_GENENRATOR(ref, neg)
+  `PHASE_DETECTOR_FLIPFLOP_GENENRATOR(pll, pos)
+  `PHASE_DETECTOR_FLIPFLOP_GENENRATOR(pll, neg)
+
+
+  `undef PHASE_DETECTOR_FLIPFLOP_GENENRATOR
+
+  always_comb
+    comb_arst_ni = arst_ni & (~(
+                                (clk_ref_posedge & clk_pll_posedge) |
+                                (clk_ref_negedge & clk_pll_negedge)
+                              ));
+
+  always_comb freq_incr_o = clk_ref_posedge | clk_ref_negedge;
+  always_comb freq_decr_o = clk_pll_posedge | clk_pll_negedge;
 
 endmodule
